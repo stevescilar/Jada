f4ae6c7e96956f9d9027b9f7242244354db1fbb1
stevescilar <stevemuambi@gmail.com>
2023-11-07 13:21:23 +0300

    verify user via email

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
 $request->fulfill();
 
-return redirect('/dashboard');
+return redirect('/login');
 })->middleware(['auth', 'signed'])->name('verification.verify');
 
 // register routes
