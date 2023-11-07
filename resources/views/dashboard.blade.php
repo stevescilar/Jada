4ba36ff2739a4226ec089807940dd9bb17832c82
stevescilar <stevemuambi@gmail.com>
2023-11-07 11:54:12 +0300

    dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,13 +1,66 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            Hello, {{ auth()->user()->name }}
+            <p>Your trial ends on <strong>{{ auth()->user()->user_trial }}</strong> </p>
 
-<div class="container mt-5">
-    <div class="row justify-content-center">
-        {{ auth()->user()->name }}
-        {{ auth()->user()->email }}
-        <div class="col-md-8">
+            <div class="col-md-3">
+                <div class="card-counter primary">
+                    <p class="text-center mt-3 lead">User Profile</p>
+                    <button class="btn btn-primary float-end">View</button>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card-counter danger">
+                    <p class="text-center mt-3 lead">Post Job</p>
+                    <button class="btn btn-primary float-end">View</button>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card-counter success">
+                    <p class="text-center mt-3 lead">Job Listings</p>
+                    <button class="btn btn-primary float-end">View</button>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card-counter info">
+                    <p class="text-center mt-3 lead">Item 4</p>
+                    <button class="btn btn-primary float-end">View</button>
+                </div>
+            </div>
         </div>
     </div>
-</div>
+
+    <style>
+    .card-counter {
+        box-shadow: 2px 2px 10px #DADADA;
+        margin: 5px;
+        padding: 20px 10px;
+        background-color: #fff;
+        height: 130px;
+        border-radius: 5px;
+        transition: .3s linear all;
+    }
+    .card-counter.primary {
+        background-color: #007bff;
+        color: #FFF;
+    }
+    .card-counter.danger {
+        background-color: #ef5350;
+        color: #FFF;
+    }
+    .card-counter.success {
+        background-color: #66bb6a;
+        color: #FFF;
+    }
+    .card-counter.info {
+        background-color: #26c6da;
+        color: #FFF;
+    }
+</style>
 @endsection
